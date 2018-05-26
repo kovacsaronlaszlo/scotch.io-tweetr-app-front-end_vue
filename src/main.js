56fbf3b20b0c57ef4c7d6830267f13d317ad0ff7
kovacs.aron <thahWeiJe7co>
2018-05-26 20:31:58 +0200

    [add]: + add semantic cdn + vee-validate for validation + some changes in css and view

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import axios from 'axios'
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
 import App from './App'
 import router from './router'
-import axios from 'axios'
+
 
 // axios for reach database
 window.axios = axios
 axios.defaults.baseURL = 'http://127.0.0.1:3333'
+
+Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
 
