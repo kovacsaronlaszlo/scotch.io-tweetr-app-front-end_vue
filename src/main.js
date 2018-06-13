a78f6b8cb4b6222f52fb34ccb43e5d72dbf0d8e2
Kovács Áron László <k.aron.laszlo@gmail.com>
2018-06-13 20:26:33 +0200

    [create]: + install moment.js + tweet and tweets components

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Vue from 'vue'
 import VeeValidate from 'vee-validate'
 import App from './App'
 import router from './router'
+import moment from 'moment'
 
 // reference axios globally
 window.axios = axios
@@ -16,6 +17,11 @@ axios.defaults.baseURL = 'http://127.0.0.1:3333'
 Vue.use(VeeValidate)
 
 Vue.config.productionTip = false
+
+// globális szűrők a dátumok formázásához a moment js-sel
+Vue.filter('timeAgo', date => moment(date).fromNow())
+Vue.filter('joined', date => moment(date).format('MMMM YYYY'))
+Vue.filter('dob', date => moment(date).format('MMMM Do YYYY'))
 
 /* eslint-disable no-new */
 new Vue({
